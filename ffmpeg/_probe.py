2e6ab4c3e857d40a1fea99f28b7ff7fc6742812f
Mikko Vatanen <mikko.vatanen@csc.fi>
2019-11-22 15:57:28 +0200

    KDKPAS-2098 Add lossless detection for JPEG2000 files

--- ffmpeg/_probe.py
+++ ffmpeg/_probe.py
@@ -21,7 +21,46 @@ def probe(filename, cmd='ffprobe', **kwargs):
     out, err = p.communicate()
     if p.returncode != 0:
         raise Error('ffprobe', out, err)
-    return json.loads(out.decode('utf-8'))
+    return _add_stream_properties(json.loads(out.decode('utf-8')), err)
+
+
+def _add_stream_properties(properties, err):
+    """Parse stream properties from ffprove stderr output, and return as
+    dictionary.
+
+    Currently ffprobe does not return some attributes in the result JSON, which
+    requires us to parse some attributes from the stderr output.
+
+    This is temporary patch, because stderr output is not well defined. Proper
+    patch should be sent to ffmpeg developers, to add required attributes to
+    ffmpeg standard output.
+
+    """
+    for stream in properties["streams"]:
+        if "codec_name" in stream and stream["codec_name"] == "jpeg2000":
+            stream["lossless_wavelet_transform"] = _determine_quality(
+                err, stream["index"])
+    return properties
+
+
+def _determine_quality(stderr_contents, stream_index):
+    """
+    Determine whether the wavelet transform of JPEG2000 stream was lossless.
+
+    This is done by examining the line corresponding to the relevant stream and
+    determining whether it contains the string "lossless".
+
+    :stderr_contents: The stderr output of ffprobe.
+    :stream_index: Index of the stream to be inspected.
+    :returns: None if the stream is not a JPEG2000, otherwise True for lossless
+              wavelet transform and False for lossy.
+    """
+    lines = stderr_contents.split(b"\n")
+    for line in lines:
+        if b"Stream #0:" + bytes(stream_index) in line:
+            if b"Video: jpeg2000" in line:
+                return b"lossless" in line
+    return None
 
 
 __all__ = ['probe']
